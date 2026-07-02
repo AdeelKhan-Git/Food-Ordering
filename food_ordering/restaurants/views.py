ad54a8848ec43d3910855ad44a6ec803bfebb5f2
AdeelKhan-Git <engr.adeelkhan2002@gmail.com>
2026-07-02 11:20:22 +0500

    added CRUD for menu items

--- food_ordering/restaurants/views.py
+++ food_ordering/restaurants/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status,permissions
+from rest_framework.exceptions import ValidationError
 from restaurants.models import Restaurants, Category,MenuItem,Deal,DealItem
-from restaurants.serializer import CategorySerializer,AllCategorySerializer,AllRestaurantSerializer ,RestaurantSerializer, MenuItemSerializer,DealItemSerializer, DealSerializer
+from restaurants.serializer import CategorySerializer,AllCategorySerializer,AllRestaurantSerializer ,RestaurantSerializer, MenuItemSerializer,AllMenuItemSerializer,DealItemSerializer, DealSerializer
 from drf_yasg.utils import swagger_auto_schema
 
 # Create your views here.
@@ -26,8 +27,10 @@ class CreateCategoryView(APIView):
             serializer.save(created_by= request.user)
 
             return Response({"message":"Category Added","data":serializer.data}, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"error":e.detail},status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 
 class GetCategoryView(APIView):
@@ -70,10 +73,11 @@ class UpdateCategoryView(APIView):
             category = Category.objects.select_related('created_by').get(id =cat_id) 
             serializer = CategorySerializer(category, data=request.data, partial = True)
             serializer.is_valid(raise_exception=True)
-            serializer.save(created_by = request.user)
+            serializer.save(updated_by = request.user)
 
             return Response({"message":"Category Updated","data":serializer.data},status=status.HTTP_200_OK)
-
+        except ValidationError as e:
+            return Response({"error":e.detail},status=status.HTTP_400_BAD_REQUEST)
         except Category.DoesNotExist:
             return Response({"error":"Category not Found"},status=status.HTTP_404_NOT_FOUND) 
         except Exception as e:
@@ -112,6 +116,8 @@ class CreateRestuarantView(APIView):
 
             serializer.save(created_by =request.user)
             return Response({"message":"Resturant Added","data":serializer.data}, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"error":e.detail},status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
 
@@ -145,9 +151,11 @@ class UpdateRestaurantView(APIView):
 
             serailizer = RestaurantSerializer(restaurant, data=request.data, partial =True)
             serailizer.is_valid(raise_exception=True)
-            serailizer.save(created_by = request.user)
+            serailizer.save(updated_by = request.user)
 
             return Response({"message":"Restaurant Updated","data":serailizer.data},status=status.HTTP_200_OK)
+        except ValidationError as e:
+            return Response({"error":e.detail},status=status.HTTP_400_BAD_REQUEST)
         except Restaurants.DoesNotExist:
             return Response({"error":"Restaurant not Found"},status=status.HTTP_404_NOT_FOUND) 
         except Exception as e:
@@ -171,20 +179,75 @@ class DeleteRestaurantView(APIView):
 #---------------Menu Item----------------------
 
 class CreateMenuItemView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    @swagger_auto_schema(
+            request_body=MenuItemSerializer,
+            responses={201: MenuItemSerializer}
+    )
     def post(self, request):
         try:
             serialzer = MenuItemSerializer(data = request.data)
             serialzer.is_valid(raise_exception=True)
             serialzer.save(created_by = request.user)
             return Response({"Message":"Menu Item Added","data":serialzer.data}, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({"error":e.detail},status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error":e},status=status.HTTP_400_BAD_REQUEST)
 
 class GetAllMenuItemsView(APIView):
     def get(self, request):
         try:
-            menu_items = MenuItem.objects.select_related('created_by').only()
+            menu_items = MenuItem.objects.select_related('created_by','restaurant_id','category_id').only('id','name','created_by','restaurant_id','category_id')
+
+            serializer = AllMenuItemSerializer(menu_items,many=True)
+            return Response({"data":serializer.data},status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
+        
+class GetMenuItemView(APIView):
+    def get(self,request,menu_id):
+        try:
+            menu = MenuItem.objects.select_related('restaurant_id','category_id').get(id=menu_id)
+            serializer = MenuItemSerializer(menu)
+
+            return Response({"data":serializer.data}, status=status.HTTP_200_OK)
+        except MenuItem.DoesNotExist:
+            return Response({"error":"Menu Item not Found"},status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
 
-    
+class UpdateMenuItemView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    @swagger_auto_schema(
+            request_body=MenuItemSerializer,
+            responses={201: MenuItemSerializer}
+    )
+    def patch(self, request,menu_id):
+        try:
+            menu = MenuItem.objects.get(id=menu_id)
+
+            serializer = MenuItemSerializer(menu, data = request.data, partial =True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(updated_by = request.user)
+            return Response({"message":"Updated Menu Item","data":serializer.data}, status=status.HTTP_200_OK)
+        except ValidationError as e:
+            return Response({"error":e.detail},status=status.HTTP_400_BAD_REQUEST)
+        except MenuItem.DoesNotExist:
+            return Response({"error":"Menu Item not Found"},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
+
+class DeleteMenuItemView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    def delete(self, request, menu_id):
+        try:
+            menu = MenuItem.objects.get(id=menu_id)
+            serializer = MenuItemSerializer(menu)
+            menu.delete()
+            return Response({"message":"Menu Item Deleted","data":serializer.data}, status=status.HTTP_200_OK)
+        except MenuItem.DoesNotExist:
+            return Response({"error":"Menu Item not Found"},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_400_BAD_REQUEST)
