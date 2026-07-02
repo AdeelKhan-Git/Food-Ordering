9b09644d53857ef2a5a0113b3074c8cc6792e71f
AdeelKhan-Git <engr.adeelkhan2002@gmail.com>
2026-07-02 11:19:54 +0500

    added menu items urls

--- food_ordering/restaurants/urls.py
+++ food_ordering/restaurants/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from restaurants.views import GetCategoryView, CreateCategoryView,GetAllCategoryView,UpdateCategoryView,DeleteCategoryView,CreateRestuarantView,GetAllRestaurantView,UpdateRestaurantView,GetRestaurantView,DeleteRestaurantView
+from restaurants.views import GetCategoryView, CreateCategoryView,GetAllCategoryView,UpdateCategoryView,DeleteCategoryView,CreateRestuarantView,GetAllRestaurantView,UpdateRestaurantView,GetRestaurantView,DeleteRestaurantView,CreateMenuItemView,GetAllMenuItemsView,GetMenuItemView,UpdateMenuItemView,DeleteMenuItemView
 
 urlpatterns = [
    #----- category 
@@ -15,5 +15,12 @@ urlpatterns = [
    path('all-restaurant',GetAllRestaurantView.as_view(), name = 'all-restaurant'),
    path('update-restaurant/<int:rest_id>/',UpdateRestaurantView.as_view(), name = 'update-restaurant'),
    path('delete-restaurant/<int:rest_id>/',DeleteRestaurantView.as_view(), name = 'delete-restaurant'),
+
+   #--------- Menu items
+   path('create-menuitem/',CreateMenuItemView.as_view(), name ='create-menuitem'),
+   path('menuitem/<int:menu_id>',GetMenuItemView.as_view(), name ='get-menuitem'),
+   path('all-menuitem',GetAllMenuItemsView.as_view(), name = 'all-menuitem'),
+   path('update-menuitem/<int:menu_id>/',UpdateMenuItemView.as_view(), name = 'update-menuitem'),
+   path('delete-menuitem/<int:menu_id>/',DeleteMenuItemView.as_view(), name = 'delete-menuitem'),
 
 ]
