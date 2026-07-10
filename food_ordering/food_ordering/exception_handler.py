42a2dc74830a98b47793a85382a2906624fa10ca
AdeelKhan-Git <engr.adeelkhan2002@gmail.com>
2026-07-10 11:38:24 +0500

    update exception handler

--- food_ordering/food_ordering/exception_handler.py
+++ food_ordering/food_ordering/exception_handler.py
@@ -1,25 +1,27 @@
 from rest_framework.views import exception_handler
+from rest_framework.exceptions import ErrorDetail
+
+def format_error(value):
+    if isinstance(value, dict):
+        return {k: format_error(v) for k, v in value.items()}
+
+    if isinstance(value, list):
+        if len(value) == 1:
+            return format_error(value[0])
+        return [format_error(v) for v in value]
+
+    if isinstance(value, ErrorDetail):
+        return str(value)
+
+    return value
+
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if response is not None:
-        errors = response.data
-
-        if isinstance(errors, dict):
-            formatted_errors = {}
-
-            for field, messages in errors.items():
-                if isinstance(messages, list):
-                    formatted_errors[field] = str(messages[0])  # first error only
-                else:
-                    formatted_errors[field] = str(messages)
-
-            response.data = {"error":formatted_errors}
-
-        elif isinstance(errors, list):
-            response.data = {
-                "error": str(errors[0])
-            }
+        response.data = {
+            "error": format_error(response.data)
+        }
 
     return response
