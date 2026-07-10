f6bb30b7be9632951818eaf93f229cc335e4a88a
AdeelKhan-Git <engr.adeelkhan2002@gmail.com>
2026-07-10 09:22:58 +0500

    added exception_handler.py for custom exceptions

---
+++ food_ordering/food_ordering/exception_handler.py
@@ -0,0 +1,25 @@
+from rest_framework.views import exception_handler
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+
+    if response is not None:
+        errors = response.data
+
+        if isinstance(errors, dict):
+            formatted_errors = {}
+
+            for field, messages in errors.items():
+                if isinstance(messages, list):
+                    formatted_errors[field] = str(messages[0])  # first error only
+                else:
+                    formatted_errors[field] = str(messages)
+
+            response.data = formatted_errors
+
+        elif isinstance(errors, list):
+            response.data = {
+                "error": str(errors[0])
+            }
+
+    return response
